5227afa8b37fa000b5c3a6d7c36ddc2cfed959af
brayangpx <posadasbrayanweb@gmail.com>
2026-05-31 17:12:56 -0600

    feat(routes): add application routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
+<?php
+
 use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\AuthController;
@@ -8,8 +10,10 @@ use App\Http\Controllers\HeatmapController;
 use App\Http\Controllers\NotificationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+Route::get('/', fn () => redirect()->route('login'));
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'autenticar'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
@@ -19,9 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/appointments/search', [AppointmentController::class, 'search'])->name('appointments.search');
     Route::get('/appointments/create', [AppointmentController::class, 'create'])->name('appointments.create');
     Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
+    Route::post('/appointments/{sharedId}/status', [AppointmentController::class, 'updateStatus'])->name('appointments.status');
 
-    Route::post('/appointments/preview-temp', [HaircutPreviewController::class, 'generateTemp'])
-        ->name('appointments.preview-temp');
+    Route::post('/appointments/preview-temp', [HaircutPreviewController::class, 'generateTemp'])->name('appointments.preview-temp');
 
     Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
     Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
